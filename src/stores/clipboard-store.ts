c22b451fb4654822716fe305f35c498bf041da5b
ktlwap <ktlwap@gmail.com>
2023-11-05 11:30:05 +0100

    Improve clipboard history

--- src/stores/clipboard-store.ts
+++ src/stores/clipboard-store.ts
@@ -10,8 +10,6 @@ export interface ClipboardState {
   history: string[];
   add: (text: string) => void;
   clear: () => void;
-  slice: () => void;
-  pop: () => void;
 }
 
 export const useClipboardStore = create<ClipboardState>()(
@@ -23,8 +21,23 @@ export const useClipboardStore = create<ClipboardState>()(
         },
         history: [],
 
-        add: (text: string) =>
+        add: (data: string) =>
           set((state) => {
+            // Determine whether copied data already exists within history.
+            // In case it already exists bring it up to the first position.
+            const existingElement = state.history.find(
+              (other) => data === other
+            );
+            if (existingElement !== undefined) {
+              return {
+                history: [
+                  existingElement,
+                  ...state.history.filter((other) => other !== existingElement),
+                ],
+              };
+            }
+
+            // Remove element at the end in case history limit has been reached.
             if (
               state.history.length >
               state.configuration.maxNumberOfEntries + 1
@@ -33,22 +46,10 @@ export const useClipboardStore = create<ClipboardState>()(
             }
 
             return {
-              history: [text, ...state.history],
+              history: [data, ...state.history],
             };
           }),
         clear: () => set((_) => ({ history: [] })),
-        slice: () =>
-          set((state) => {
-            const temp = state.history;
-            temp.slice();
-            return temp;
-          }),
-        pop: () =>
-          set((state) => {
-            const temp = state.history;
-            temp.pop();
-            return temp;
-          }),
       }),
       {
         name: "bear-storage",
